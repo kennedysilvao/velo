7750ae8dc1a57c54edd6d2f0e35eaf4810df9054
Kennedy Oliveira <kennedysilva20081@gmail.com>
2026-04-20 15:11:40 -0300

    Padrão AAA

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,12 +1,18 @@
 import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+    // Arrange
     await page.goto('http://localhost:5173/')
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+    // Act
     await page.getByTestId('search-order-id').fill('VLO-QL43NG')
     await page.getByTestId('search-order-button').click()
+
+    // Assert
     await expect(page.getByTestId('order-result-id')).toContainText('VLO-QL43NG')
     await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 });
