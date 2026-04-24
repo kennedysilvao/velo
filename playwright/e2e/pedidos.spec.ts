e9e8558fe689eb64b81a6827abf7abb68464a4e3
Kennedy Oliveira <kennedysilva20081@gmail.com>
2026-04-24 17:36:05 -0300

    adicionando cenário de pedido não encontrado

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -2,14 +2,18 @@ import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
 
+    // Test Data
+    const orderId = 'VLO-QL43NG'
+    
     // Arrange
+
     await page.goto('/')
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
     // Act
-    await page.getByTestId('search-order-id').fill('VLO-QL43NG')
+    await page.getByTestId('search-order-id').fill(orderId)
     await page.getByTestId('search-order-button').click()
 
     // Assert
@@ -21,7 +25,30 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
         .filter({ hasText: /^Pedido$/ })
         .locator('..')
 
-    await expect(containerPedido).toContainText('VLO-QL43NG', {timeout: 10_000})
+    await expect(containerPedido).toContainText(orderId, {timeout: 10_000})
     await expect(page.getByText('APROVADO')).toBeVisible();
     await expect(page.locator('//div[text()="APROVADO"]')).toContainText('APROVADO')
-});
+})
+
+test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+    // Test Data
+    const orderId = 'VLO-QL33NG'
+    
+    // Arrange
+
+    await page.goto('/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+    // Act
+    await page.getByTestId('search-order-id').fill(orderId)
+    await page.getByTestId('search-order-button').click()
+
+    // Assert
+    const title = page.getByRole('heading', {level: 3, name: 'Pedido não encontrado'})
+    await expect(title).toBeVisible()
+
+    const message = page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'})
+    await expect(message).toBeVisible()
+})
