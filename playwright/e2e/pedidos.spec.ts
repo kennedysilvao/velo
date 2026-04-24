97528da77cfda9b3ee36e51c73bcecd766ac235e
Kennedy Oliveira <kennedysilva20081@gmail.com>
2026-04-24 18:39:02 -0300

    adicionando validação de snapshot

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,7 +4,6 @@ import { generateOrderId } from '../support/helpers'
 
 
 test.describe('Consulta de Pedido', () => {
-
     test.beforeEach(async ({ page }) => {
         // Arrange
 
@@ -24,13 +23,34 @@ test.describe('Consulta de Pedido', () => {
         await page.getByTestId('search-order-button').click()
 
         // Assert
-        const containerPedido = page.getByRole('paragraph')
-            .filter({ hasText: /^Pedido$/ })
-            .locator('..')
-
-        await expect(containerPedido).toContainText(orderId, { timeout: 10_000 })
-        await expect(page.getByText('APROVADO')).toBeVisible();
-        await expect(page.locator('//div[text()="APROVADO"]')).toContainText('APROVADO')
+        await expect(page.getByTestId(`order-result-${orderId}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${orderId}
+            - img
+            - text: APROVADO
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: Glacier Blue
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: aero Wheels
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: Kennedy Silva
+            - paragraph: Email
+            - paragraph: kennedy@velo.dev
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: À Vista
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
     })
 
     test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
