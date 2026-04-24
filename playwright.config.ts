75d5db2889f1d29d891dcdfded98a1a5eaeebdd2
Kennedy Oliveira <kennedysilva20081@gmail.com>
2026-04-23 21:03:41 -0300

    adicionando configurações globais de timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,14 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  // Tempo máximo para cada teste completo (30 segundos é o padrão)
+  timeout: 60_000,
+  
+  // Tempo máximo para assertions (toBeVisible(), toHaveText())
+  expect: {
+    timeout: 5_000
+  },
+
   testDir: './playwright/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -30,6 +38,14 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    // Tempo máximo pára ações interativas como click(), fill()
+    // Quando o valor é 0, herda do timeout padrão/customizado
+    actionTimeout: 5_000,
+
+    // Tempo máximo pára ações interativas como goto(), waitForUrl()
+    // Quando o valor é 0, herda do timeout padrão/customizado
+    navigationTimeout: 10_000
   },
 
   /* Configure projects for major browsers */
